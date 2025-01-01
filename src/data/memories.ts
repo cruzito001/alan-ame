bfb734a7aa67084d91f8f0eb025e38a1f0e07d26
cruzito001 <doctor.printer-0k@icloud.com>
2024-12-31 19:05:11 -0600

    Delete /public

--- src/data/memories.ts
+++ src/data/memories.ts
@@ -7,11 +7,7 @@ export const memories: Memories = [
     date: "2024-08-16",
     description:
       "Nuestro primer viaje a la playa juntos, increíble hacer todo contigo, convivir con tu papá y la gordis, con marcelo y kim. Gracias por ese fin de semana tan bonito a tu lado, es el primero de muchos viajes juntos amor. <3",
-    images: [
-      "/public/2024/tampico1.jpg",
-      "/public/2024/tampico2.jpg",
-      "/public/2024/tampico3.jpg",
-    ],
+    images: ["2024/tampico1.jpg", "2024/tampico2.jpg", "2024/tampico3.jpg"],
   },
   {
     id: "2",
@@ -20,10 +16,10 @@ export const memories: Memories = [
     description:
       "Aunque las fotos no son de tu mero cumpleaños, el celebrar contigo otro cumpleaños a tu lado es algo que me pone muy feliz y de verdad espero estar en todos los cumpleaños que restan de tu vida corazón mío, te amo. <3",
     images: [
-      "/public/2024/amecumple1.jpg",
-      "/public/2024/amecumple2.jpg",
-      "/public/2024/amecumple3.jpg",
-      "/public/2024/amecumple4.jpg",
+      "2024/amecumple1.jpg",
+      "2024/amecumple2.jpg",
+      "2024/amecumple3.jpg",
+      "2024/amecumple4.jpg",
     ],
   },
   {
@@ -33,10 +29,10 @@ export const memories: Memories = [
     description:
       "Nada que agregar el cumple de la gordis que tanto queremos, espero poder estar juntos cada cumple de Sofi, mua.",
     images: [
-      "/public/2024/sofiacumple1.jpg",
-      "/public/2024/sofiacumple2.jpg",
-      "/public/2024/sofiacumple3.jpg",
-      "/public/2024/sofiacumple4.jpg",
+      "2024/sofiacumple1.jpg",
+      "2024/sofiacumple2.jpg",
+      "2024/sofiacumple3.jpg",
+      "2024/sofiacumple4.jpg",
     ],
   },
   {
@@ -45,7 +41,7 @@ export const memories: Memories = [
     date: "2024-12-01",
     description:
       "Feliz de que por segundo año me acompañes a mi y a mi familia en ir a la basílica, es algo que mi mamá aprecia y me gusta que formes parte de estos planes. <3",
-    images: ["/public/2024/peregrinacion1.jpg"],
+    images: ["2024/peregrinacion1.jpg"],
   },
   {
     id: "5",
@@ -53,7 +49,7 @@ export const memories: Memories = [
     date: "2024-12-08",
     description:
       "Otro viajecito juntos, no fue playa pero fue un lugar y experiencia muy bonita. ¿Se volverá a repetir? no lo sé, depende de ti y de como quieras dormir jajaja, te amo <3.",
-    images: ["/public/2024/concha1.jpg", "/public/2024/concha2.jpg"],
+    images: ["2024/concha1.jpg", "2024/concha2.jpg"],
   },
   {
     id: "6",
@@ -61,6 +57,6 @@ export const memories: Memories = [
     date: "2024-12-24",
     description:
       "Segunda navidad de muchas juntos, te amo <3 pd. ya quiero que la hagamos de Santa Claus, mua <3",
-    images: ["/public/2024/navidad1.jpg", "/public/2024/navidad2.jpg"],
+    images: ["2024/navidad1.jpg", "2024/navidad2.jpg"],
   },
 ];
